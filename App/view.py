ab452c58f1488a881e77251b8766a6814039b55a
DanielaAlvarez1 <d.alvarezr@uniandes.edu.co>
2021-02-13 14:50:41 -0500

    cambios carga datos

--- App/view.py
+++ App/view.py
@@ -60,6 +60,7 @@ while True:
         print("Videos cargados: " + str(lt.size(catalog['videos'])))
         t2 = time.process_time()
         print("El tiempo de ejecución fue de " + str(t2-t1) + " segundos")
+        print(catalog["videos"])
 
     elif int(inputs[0]) == 2:
         t1 = time.process_time()
